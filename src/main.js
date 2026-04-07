a06c3d561d914e1170263ce75e8099239108b3ca
Erin Sullivan <sulliv.erin@gmail.com>
2026-04-07 09:28:02 -0400

    Disabling `no-console` rule violation for this file since it's a demo and we want to log the results to the console.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ const mrDarcy = new Prejudice();
 // MrDarcy.setRecordStorage(Prejudice.VariableStorageDriver);
 mrDarcy.addRecord('12345');
 mrDarcy.act('text', '1234567890', (info) => {
+  // eslint-disable-next-line no-console
   console.log(info);
 });
 mrDarcy.clearRecords();
