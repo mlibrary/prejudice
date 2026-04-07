c0c01a2ce93bc86ff3afc53d4b737c1c8bba9f49
Erin Sullivan <sulliv.erin@gmail.com>
2026-04-07 10:00:56 -0400

    Ignoring `camelcase`, `class-methods-use-this`, `func-names`, `max-params`, `no-param-reassign`, `no-shadow` eslint rule violations. Made a note to resolve in the future.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,9 +27,21 @@ export default [
       '@stylistic/space-before-function-paren': ['error', 'always'],
       '@stylistic/spaced-comment': ['error', 'always', { block: { balanced: true } }],
       'arrow-body-style': ['error', 'always'],
+      // Eventually resolve:
+      camelcase: 'off',
+      // Eventually resolve:
+      'class-methods-use-this': 'off',
+      // Eventually resolve:
+      'func-names': 'off',
       'max-lines-per-function': 'off',
+      // Eventually resolve:
+      'max-params': 'off',
       'max-statements': 'off',
       'no-magic-numbers': 'off',
+      // Eventually resolve:
+      'no-param-reassign': 'off',
+      // Eventually resolve:
+      'no-shadow': 'off',
       'no-ternary': 'off',
       'one-var': ['error', { initialized: 'never' }],
       'sort-imports': ['error', { ignoreCase: true }]
